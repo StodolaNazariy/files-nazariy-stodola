f046d1c504f53968e6d236cef6f7b37c1af93298
Nazariy <onepiece.nazar@gmail.com>
2022-10-09 22:06:57 +0300

    change package structure

---
+++ libs/validateService.js
@@ -0,0 +1,30 @@
+const validationService = {
+  name: name => {
+    if (name.length < 3 || name.length > 30) {
+      return `Name should contain at least 3 and max 30 characters`;
+    }
+  },
+
+  email: email => {
+    const template =
+      /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+    const isValid = template.test(email);
+    if (!isValid) {
+      return "Invalid email format";
+    }
+  },
+
+  password: password => {
+    if (password.length < 8 || password.length > 30) {
+      return `Password should contain at least 8 and max 32 characters`;
+    }
+
+    const template = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/;
+    const isValid = template.test(password);
+    if (!isValid) {
+      return "Password should contain at least 1 lowercase, 1 uppercase, and 1 number.";
+    }
+  },
+};
+
+module.exports = validationService;
